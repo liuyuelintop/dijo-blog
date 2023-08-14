cb4c7278bdec43eef104b355345e4341c88040ce
YuelinLiu_22 <liuyuelin@YuelinMac.local>
2023-08-14 15:02:38 +1000

    useEffect Dependencies

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,21 +5,23 @@ const Home = () => {
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-      ])
-    
-      const handleDelete = (id) => {
+    ])
+    const [name, setName] = useState("mario")
+    const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs)
-      }
+    }
     
     // rerender trigers hit function
     useEffect(() =>{
         console.log('use effect ran')
-        console.log(blogs)
-    })
+        console.log(name)
+    }, [name])
     return (  
         <div className="home">
-          <BlogList blogs={blogs} title = 'All Blogs' handleDelete = {handleDelete}/>
+            <BlogList blogs={blogs} title = 'All Blogs' handleDelete = {handleDelete}/>
+            <button onClick={() => setName('luigi')}>change name</button>
+            <p>{name}</p>
         </div>
     );
 }
