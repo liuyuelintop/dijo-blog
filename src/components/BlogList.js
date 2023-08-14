fb96c9209ea05da520835fd2c390f0224cf6c6eb
YuelinLiu_22 <liuyuelin@YuelinMac.local>
2023-08-14 14:51:11 +1000

    lesson 13, functions as props

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = ({blogs, title}) => {
+const BlogList = ({blogs, title, handleDelete}) => {
     // const blogs = props.blogs;
     // const title = props.title;
     // console.log(blogs);
@@ -7,9 +7,10 @@ const BlogList = ({blogs, title}) => {
       <div className="blog-list">
         <h2>{ title }</h2>
         {blogs.map(blog => (
-          <div className="blog-preview" key={blog.id} >
+          <div className="blog-preview" key={blog.id}>
             <h2>{ blog.title }</h2>
             <p>Written by { blog.author }</p>
+            <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
           </div>
         ))}
       </div>
