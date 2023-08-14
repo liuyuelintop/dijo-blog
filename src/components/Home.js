442c30a512b325a5321aec0da3b0663e41519b51
YuelinLiu_22 <liuyuelin@YuelinMac.local>
2023-08-14 14:12:23 +1000

    use State to create reactive value

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,18 @@
-const Home = () => {
+import { useState } from "react";
 
+const Home = () => {
+    // make a hook: state, which is a reactive value
     const handleClick = () => {
-        console.log('Hello, Ninja')
+        setName('luigi');
+        setAge(30)
     }
+
+    const [name, setName] = useState("mario");
+    const [age, setAge] = useState(20);
     return (  
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click me</button>
         </div>
     );
