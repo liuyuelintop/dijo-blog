5d76e7ac9af572cae3716c57e549276d75110295
YuelinLiu_22 <liuyuelin@YuelinMac.local>
2023-08-14 15:36:02 +1000

    lesson 18, Conditional Loading Message

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,28 @@
 import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
-    const [name, setName] = useState("mario")
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs)
-    }
-    
-    // rerender trigers hit function
-    useEffect(() =>{
-        console.log('use effect ran')
-        console.log(name)
-    }, [name])
-    return (  
-        <div className="home">
-            <BlogList blogs={blogs} title = 'All Blogs' handleDelete = {handleDelete}/>
-            <button onClick={() => setName('luigi')}>change name</button>
-            <p>{name}</p>
-        </div>
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+  
+    useEffect(() => {
+      setTimeout(() => {
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+          return res.json();
+        })
+        .then(data => {
+          setIsPending(false);
+          setBlogs(data);
+        })
+      }, 1000);
+    }, [])
+  
+    return (
+      <div className="home">
+        { isPending && <div>Loading...</div> }
+        {blogs && <BlogList blogs={blogs} />}
+      </div>
     );
-}
+  }
 
 export default Home;
