8be25c7edc6ec61459910686b9e1fb5bdeb21ba7
YuelinLiu_22 <liuyuelin@YuelinMac.local>
2023-08-15 13:17:38 +1000

    lesson28, Submit Events

--- src/components/Create.js
+++ src/components/Create.js
@@ -4,11 +4,16 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState(' ');
     const [author, setAuthor] = useState('mario');
-
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = {title, body, author};
+        console.log(blog)
+    }
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog Title</label>
                 <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)}/>
                 <label>Blog Body:</label>
